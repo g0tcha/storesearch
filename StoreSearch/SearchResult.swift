214c32be99e03154f8b870a3cbff332e6663725b
vincent_g <vincent.grossier@infostrates.fr>
2016-07-26 15:39:37 +0200

    Added a search bar and table view. The search puts fake resulsts in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by vincent on 26/07/2016.
+//  Copyright © 2016 infostrates. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
